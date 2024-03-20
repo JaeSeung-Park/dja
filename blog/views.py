9cf2773d98ca57def2c89cb577475003ff09ed07
Park JaeSeung <wotmd36963@gmail.com>
2024-03-20 23:44:18 +0900

    create category page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from . models import Post, Category
 
@@ -6,6 +6,7 @@ from . models import Post, Category
 class PostList(ListView):
     model = Post
     ordering = '-pk'
+    # template_name = 'blog/post_list.html'
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
@@ -13,7 +14,25 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-    # template_name = 'blog/post_list.html'
+def category_page(requset, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        requset,
+        'blog/post_list.html',
+        {
+            'post_list' : post_list,
+            'categories' : Category.objects.all(),
+            'no_category_post_count' : Post.objects.filter(category=None).count(),
+            'category' : category,
+        }
+    )
+
 
 # def index(request):
 #     posts = Post.objects.all().order_by('-pk')
